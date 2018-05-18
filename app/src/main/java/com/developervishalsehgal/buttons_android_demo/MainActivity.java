ace563e5c5997ff701895b60d7aa7317f1ce7978
Vishal Sehgal <codervishalsehgal@gmail.com>
2018-05-19 04:01:57 +0530

    Added Material Design Buttons

--- app/src/main/java/com/developervishalsehgal/buttons_android_demo/MainActivity.java
+++ app/src/main/java/com/developervishalsehgal/buttons_android_demo/MainActivity.java
@@ -1,13 +1,37 @@
 package com.developervishalsehgal.buttons_android_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mdButton;
+
+    private void initViews() {
+        mdButton = findViewById(R.id.btn_md);
+        mdButton.setOnClickListener(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.btn_md:
+                startActivity(new Intent(this, MaterialDesignButton.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/developervishalsehgal/buttons_android_demo/MaterialDesignButton.java
@@ -0,0 +1,13 @@
+package com.developervishalsehgal.buttons_android_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MaterialDesignButton extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buttons_material_design);
+    }
+}
